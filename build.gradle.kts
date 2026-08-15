663dafb69652a009c6666876e6680fe2790a3b2a
DaniDipp <dippigames@gmail.com>
2026-08-15 11:50:34 +0200

    Use SneakyPocketbase Kotlin runtime

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,12 +21,13 @@ repositories {
 }
 
 dependencies {
-	compileOnly("org.jetbrains.kotlin:kotlin-stdlib:2.2.21")
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
 	compileOnly("org.apache.logging.log4j:log4j-core:2.24.3")
 	compileOnly("me.clip:placeholderapi:2.11.6")
-	compileOnly("io.github.team-sneakymouse:sneakycharactermanager-paper:1.6.0")
-	compileOnly(files("C:\\Users\\DaniDipp\\Downloads\\1.21.4\\SneakyPocketbase-1.0.jar"))
+	compileOnly("io.github.team-sneakymouse:sneakycharactermanager-paper:1.6.0") {
+		exclude(group = "org.jetbrains.kotlin")
+	}
+	compileOnly(files("../SneakyPocketbase/build/libs/SneakyPocketbase-1.0.jar"))
 	compileOnly(fileTree("libs") {
 		include("*.jar")
 		exclude("SneakyCharacterManager-*.jar")
